0e1a52964df4899b00c0216c2c2aec8897c1a7cc
yoav0gal <yoav0gal@gmail.com>
2026-03-16 10:44:30 +0200

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, JetBrains_Mono } from "next/font/google";
+import { Geist, Geist_Mono, JetBrains_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { ConvexClientProvider } from "@/components/ConvexClientProvider";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages, getLocale } from "next-intl/server";
 import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
 const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
@@ -34,7 +36,7 @@ export default async function RootLayout({
   const direction = (new Intl.Locale(locale) as any).getTextInfo?.().direction || (locale === 'he' ? 'rtl' : 'ltr');
 
   return (
-    <html lang={locale} dir={direction} className={cn("font-mono", jetbrainsMono.variable)}>
+    <html lang={locale} dir={direction} className={cn( jetbrainsMono.variable, "font-sans", inter.variable)}>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
